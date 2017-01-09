a17b37754af51e95e5302a94e3baba7503baeadc
Viktor Richter <vrichter@posteo.de>
2017-01-09 14:30:34 +0100

    Update checkAmbiguities script. let it ignore errors

--- project/checkAmbiguities.sh
+++ project/checkAmbiguities.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+AMBIGUOUS_OUT=0
+
 RED='\033[0;31m'
 NONE='\033[0m'
 
@@ -14,7 +16,12 @@ AMBIGUITIES="$(join <(echo "$TYPES_1") <(echo "$TYPES_2"))"
 if [ "$AMBIGUITIES" != "" ]; then
   NICER="$(echo "$AMBIGUITIES" | sed "s%^%\t\\\033[0;31m%" | sed "s%$%\\\033[0m%")"
   echo -e "Ambiguous prototypes found. Already existing:\n$NICER"
-  exit 1
+  if [ "$AMBIGUOUS_OUT" == "0" ]; then
+    echo -e "\033[0;31mWARNING:\033[0m This should not be built or used when ambiguities exist. It is YOUR fault if something breaks."
+  else
+    echo -e "\033[0;31mERROR:\033[0m Will not build as long as ambiguities exist. Please clean up and update your projects.\n"
+  fi
+  exit $AMBIGUOUS_OUT
 else
   echo -e "No Ambiguities found."
   exit 0
