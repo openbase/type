cbf502e36546c00ceaa063ff1ce595de08a71b32
Divine Threepwood <divine@openbase.org>
2022-01-12 00:27:11 +0100

    update protobuf to v3.19.2

--- src/main/proto/update-packages.py
+++ src/main/proto/update-packages.py
@@ -2,7 +2,7 @@
 # #%L
 # Openbase Type Lib
 # %%
-# Copyright (C) 2018 - 2021 openbase.org
+# Copyright (C) 2018 - 2022 openbase.org
 # %%
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as
